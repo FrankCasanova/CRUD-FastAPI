48d9b36cd26a96522a4c451af26217c6e7c598c2
Frank Casanova <frankcasanova.info@gmail.com>
2024-09-26 18:45:21 +0200

    reload flag and reload_delay flag added.

--- main.py
+++ main.py
@@ -64,4 +64,4 @@ async def delete_task_endpoint(task_id: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, reload_delay=1)
